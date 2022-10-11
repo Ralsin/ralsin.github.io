41b5a5e60c51d04c17b4c1595e2601480401cf29
Ralsi <ralseineon@outlook.com>
2022-10-11 23:39:30 +0300

    .

--- src/main.js
+++ src/main.js
@@ -5,18 +5,19 @@ window.onload = () => {
     r.open('GET', 'https://discard.ralsin.ml/704037343878971424.json');
     r.onload = () => {
         var res = JSON.parse(r.response)
-        document.getElementById('dpfp').setAttribute('src', 'https://cdn.discordapp.com/avatars/' + res.id + '/' + res.avatar + '?size=1024&quality=lossless');
+        document.getElementById('dpfp').setAttribute('src', `https://cdn.discordapp.com/avatars/${res.id}/${res.avatar}?size=1024&quality=lossless`);
         document.getElementById('dpfp').removeAttribute('style');
         nickname = res.username + '#' + res.discriminator;
         nicknamE.innerText = nickname;
     }
     r.send();
+    return
 }
 function href(name) {
-    location.href = name
+    return location.href = name
 }
 function copy() {
     setTimeout(() => nicknamE.innerText = nickname, 2e3)
     navigator.clipboard.writeText(nickname)
-    nicknamE.innerText = 'Copied!'
+    return nicknamE.innerText = 'Copied!'
 }
